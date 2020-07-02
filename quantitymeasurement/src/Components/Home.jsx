49e5ce162ce13e451d9f629b23813cb7a1c0b16a
shubham <shubhamdeulkar27@gmail.com>
2020-07-02 12:31:06 +0530

    Refactored Home Component For Choosetype

--- quantitymeasurement/src/Components/Home.jsx
+++ quantitymeasurement/src/Components/Home.jsx
@@ -1,32 +1,18 @@
 import React, { Component } from "react";
 import "../CSS/Home.scss";
-import TopBar from "./TopBar";
-import "../CSS/QuantityType.scss";
-import QuantityType from "./QuantityType";
 import Conversion from "./Conversion";
 
 class Home extends Component {
   render() {
     return (
       <div className="home">
-        <TopBar />
-        <div className="home-header">
-          <span className="home-header-text">
-            Welcome To Quantity Measurement
-          </span>
+        <div className="choostypemain">
+          <div className="choostype">CHOOSE TYPE</div>
+          <div />
         </div>
-        <div className="parentchoosetype">
-          <div className="choosetype">
-            <div className="choosetype-text">CHOOSE TYPE</div>
-            <QuantityType />
-          </div>
-        </div>
-        <div>
-          <Conversion />
-        </div>
+        <Conversion />
       </div>
     );
   }
 }
-
 export default Home;
